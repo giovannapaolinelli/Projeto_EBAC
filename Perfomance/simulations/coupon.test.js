a1c5daf904ccf15b12e0f632166dcd54f95f44e4
Giovanna <giovanna.paolinelli@gmail.com>
2023-09-30 18:27:02 -0300

    add performance tests

---
+++ Perfomance/simulations/coupon.test.js
@@ -0,0 +1,23 @@
+import { group } from 'k6';
+import Coupon from '../requests/coupon.request';
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 10 },
+        { duration: '20s', target: 20 },
+        { duration: '10s', target: 0 }
+    ],
+    thresholds:{
+        http_req_duration: ['p(90) < 2000']
+    }
+}
+
+export default function () {
+    let code = Math.floor(Math.random() * 10000000000)
+    let coupon = new Coupon()
+
+    group('coupon', ()=>{
+        coupon.list("Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy")
+        coupon.post("Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy", code, 10, "fixed_product", "Cupom de desconto de teste")
+    })
+}
---
+++ Perfomance/utils/utils.js
@@ -0,0 +1,5 @@
+export default class Utils {
+    static getBaseUrl(){
+            return `http://lojaebac.ebaconline.art.br/wp-json/wc/v3`
+    }
+}
